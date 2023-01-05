c9f83173e36560fdd2e1c683d8fa3709b764a856
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-05 15:25:26 +0000

    Update Contact.jsx and Contacts.jsx

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -8,6 +8,9 @@ import { Filter } from 'components/Filter';
 import { Loader } from 'components/Loader';
 import { ContactList } from 'components/ContactList';
 import { Box } from '@mui/material';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 export default function Contactc() {
   const dispatch = useDispatch();
@@ -33,7 +36,16 @@ export default function Contactc() {
         <ContactForm />
       </Section>
       <Section title="Contacts">
-        <Filter />
+                <Filter />
+                
+                    <Stack direction="row" spacing={2} sx={{ml: 'auto'}}>
+      <Button size="large" variant="text" startIcon={<AddBoxIcon />}>
+        Add
+      </Button>
+
+    </Stack>
+    
+
         <ContactList />
         {isLoading && !error && <Loader />}
       </Section>
