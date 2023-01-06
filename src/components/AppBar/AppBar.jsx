edb9c9aa913333455065c981bb7323e6d0643d7f
Serhii Lapchenko <lapchenko.ss@gmail.com>
2023-01-06 10:21:28 +0000

    Update 4 files

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import { Box, Toolbar } from '@mui/material';
-import ContactsIcon from '@mui/icons-material/Contacts';
 import { Navigation } from '../Navigation';
 import { UserMenu } from '../UserMenu';
 import { AuthNav } from '../AuthNav';
@@ -27,13 +26,6 @@ export const Header = () => {
               alignItems: 'center',
             }}
           >
-            <ContactsIcon
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            />
             <Navigation />
           </Box>
           {isLoggedIn ? <UserMenu /> : <AuthNav />}
